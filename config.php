a106697da58e741985a42ef67c01cfc1b462a97d
Kurenai Network <96333824+Kurenai-Network@users.noreply.github.com>
2022-01-26 09:11:39 +0800

    Update config.php
    尴尬

--- config.php
+++ config.php
@@ -9,22 +9,22 @@
 
 function get_config(){
     $subscribe_url = array(
-        'a8f3.571104823.xyz',
-        '4a78.571104823.xyz',
-        '4b63.571104823.xyz',
-        'a1f3.571104823.xyz',
-        '7c3d.571104823.xyz',
-        'e971.571104823.xyz'
+        '',
+        '',
+        '',
+        '',
+        '',
+        ''
     );
     return array(
-        'api_key' => 'e615c940-77e6-4a5b-bc99-2373f2ea0b91',
+        'api_key' => '',
         'mysql_host' => '127.0.0.1',
-        'mysql_pass' => 'DeHnDRNYpGet7hrH',
-        'mysql_user' => 'ssmanage',
+        'mysql_pass' => '',
+        'mysql_user' => '',
         'redis_pass' => '',
-        'redis_host' => '127.0.0.1',
-        'redis_port' => '6379',
-        'admin_username' => 'kurenai',
+        'redis_host' => '',
+        'redis_port' => '',
+        'admin_username' => '',
         'subscribe_url' => $subscribe_url[array_rand($subscribe_url,1)]
     );
 }
